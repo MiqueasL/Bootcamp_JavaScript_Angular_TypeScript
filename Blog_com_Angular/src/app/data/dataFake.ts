35f6ef12049161181818a9b95db8de5d97fded1c
MiqueasL <114825728+MiqueasL@users.noreply.github.com>
2023-11-30 11:19:17 -0300

    Up

---
+++ Blog_com_Angular/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Bee in the world",
+    "description": "Bees pollinate the world",
+    "photoCover": "https://cdn.pixabay.com/photo/2021/08/03/07/02/bee-6518669_640.jpg"
+  },
+  {
+    "id":"2",
+    "title": "The hive",
+    "description": "Bees working in the hive",
+    "photoCover":"https://cdn.pixabay.com/photo/2014/02/27/20/59/bees-276190_640.jpg"
+  }
+]
